6481f8ca74180c5a6a8e6e541caef60a9bd9a27a
Kimsu <betnwjd25@gmail.com>
2023-09-17 18:12:53 +0900

    C언어 주석

---
+++ solution/ch03/prj01/01comments.c
@@ -0,0 +1,20 @@
+/*
+  솔루션/ 프로젝트/ 소스파일: ch03/ prj01/ 01comments.c
+  C 프로그램의 기초: 주석, 문장 키워드 공부
+  V 1.0
+*/
+
+# include <stdio.h>
+
+// 운영체제가 호출하는 함수, void로 매개변수가 없음을 표시
+int main(void)
+{
+  puts("첫 C프로그램!\n");
+
+  printf("키워드: int void return 등\n");
+  printf("식별자: main puts printf 등\n");
+
+  return 0;
+}
+
+// \n은 new line으로 한줄을 띄어쓴다.
